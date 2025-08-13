32222d4780f22d8c2a499bdecf3b905c938faebd
Arpan Neupane <105156973+arpanneupane75@users.noreply.github.com>
2025-08-14 02:16:44 +0545

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -53,7 +53,7 @@ const Hero = () => {
           {/* Buttons */}
           <div className="hero-buttons d-flex justify-content-center justify-content-md-start gap-3 flex-wrap">
             <a
-              href="/Updated_CV.pdf"
+              href="/Final_CV_2.pdf"
               className="btn btn-primary btn-lg shadow-sm hero-btn"
               download
               target="_blank"
